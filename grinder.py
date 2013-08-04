0f17abbc490fc7888e8f144edb929380c2c37cd3
Jonathan Ballands <jballands@gmail.com>
2013-08-03 21:13:05 -0500

    The pool and threading stuff should be fine now… onto testing!

--- grinder.py
+++ grinder.py
@@ -12,21 +12,65 @@
 # to a student's schedule.
 #
 
-import workerpool
+import threading, copy
 
 class GruntPool:
 
-	# Initializes a worker pool that grinds courses.
-	# @param workers: The number of workers in the worker pool you want.
-	def __init__(self, workers):
-		self.pool = workerpool.WorkerPool(workers)
+	# Initializes a grunt pool that grinds courses.
+	# @param size: The number of grunts in the pool.
+	# @param rate: The evaluation rate that all Grunts in the pool should adhere to.
+	def __init__(self, rate):
+		self.listSemaphore = threading.Semaphore()
+		self.grunts = []
+		self.runningJobs = []
+		self.doneJobs = []
+		self.checkRate = rate
 
-class CoarseGrindJob(workerpool.Job):
+	# Adds a grunt to the pool.
+	# @param dictionary: A dictionary that allows the Grunt to do work.
+	def releaseGrunt(self, dictionary):
+		thisGrunt = Job(dictionary, self.checkRate, self.runningJobs, self.doneJobs, self.listSemaphore)
+		thisGrunt.start()
+		self.grunts.append(thisGrunt)
+
+		self.listSemaphore.acquire()
+		self.prettyList.append(dictionary["classNumber"])
+		self.listSemaphore.release()
+
+	# Shuts down this Grunt pool.
+	def shutdown(self):]
+		for grunt in self.readyGrunts:
+			grunt.stop()
+			grunt.join()
+
+	# Gets a list of all the running Grunts running in a pretty format,
+	# suitable for printing.
+	def getRunningList(self):
+		return self.runningJobs
+
+	def getDoneList(self):
+		self.listSemaphore.acquire()
+		copyList = copy.copy(doneJobs)
+		doneJobs = []
+		self.listSemaphore.release()
+		return copyList
+
+class Job(threading.Thread):
 
 	# Initializes a job that a worker thread can perform.
 	# @param dictionary: A dictionary that allows the job to run.
-	def __init__(self, dictionary):
-		self.workItems = dictionary
+	# @param rate: The evaluation rate this job adheres to.
+	# @param running: A list of all the running jobs.
+	# @param done: A list of all the done jobs.
+	# @param semaphore: The list semaphore.
+	def __init__(self, dictionary, rate, running, done, semaphore):
+		self.jobItems = dictionary
+		self.stopEvent = threading.Event()
+		self.checkRate = rate
+		self.runningJobs = running
+		self.doneJobs = done
+		checkRateSemaphore = threading.Semaphore()
+		self.listSemaphore = semaphore
 
 	# See Issue #2 on GitHub, as this function contains a known bug!!
 	# Runs the job, endlessly checking to see if a course is ready to add.
@@ -36,11 +80,16 @@ class CoarseGrindJob(workerpool.Job):
 		# Endlessly check for an open seat
 		while (hasOpenSeat == False):
 
-			self.browser.submitToTimetable(dictionary["term"], dictionary["crn"])
+			self.browser.submitToTimetable(jobItems["term"], jobItems["crn"])
 			results = self.browser.locateAndParseTimetableResults()
 			if (results["full"] == None):
 				hasOpenSeat == True
-
+			checkRateSemaphore.acquire()
+			for (x in range(0, checkRate * 2)):
+				time.sleep(0.5)
+				if self.stopEvent.isSet():
+					return
+			checkRateSemaphore.release()
 
 		# Quickly try to add the course
 		self.browser.jumpToRegAndSch()
@@ -48,24 +97,18 @@ class CoarseGrindJob(workerpool.Job):
 		# BUGGY LINE
 		self.browser.submitToDropAdd(dictionary["crn"])
 
-class Grunts:
+		self.listSemaphore.acquire()
+		self.runningJobs.remove(jobItems["classNumber"])
+		self.doneJobs.append(jobItems["classNumber"])
+		self.listSemaphore.release()
 
-	# Initializes a collection of Grunts that contain data to help them do work.
-	# @param master: A browser that has already navigated to the timetable page.
-	# @param rate: The evaluation rate that the grunts run by.
-	def __init__(self, master, rate):
+	# Stops this job.
+	def stop(self):
+		self.stopEvent.set()
+
+	# Changes the check rate of this job.
+	# @param rate: The new rate.
+	def changeRate(self, rate):
+		self.checkRateSemaphore.acquire()
 		self.checkRate = rate
-		self.browser = master
-		self.list = []
-
-	# Adds a grunt to the grunt collection.
-	# @param crn: The crn the grunt pertains to.
-	# @param term: The term the grunt pertains to.
-	def addGrunt(self, crn, term):
-		thisGrunt = {"crn": crn, "term": term, "rate": self.checkRate}
-		self.list.append(thisGrunt)
-
-	# Removes a grunt from the grunt collection.
-	# @param grunt: The grunt to remove.
-	def removeGrunt(self, grunt):
-		self.list.remove(grunt)
+		self.checkRateSemaphore.release()
